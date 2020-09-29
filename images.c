2974f867a5a399facd9b69c5b3eb0c180d07576f
Naexys <naexys.naexys@gmail.com>
2020-09-29 14:25:08 +0200

    Working on images, added grayscale, values. TODO: size

--- images.c
+++ images.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct image {
-    FILE *file;
-    int dimensions[2];
+    size_t ndim;
+    size_t dimensions[2]; //size_t ?
+    double *values;
 };
 
 int file_header[2] = {66, 77};
@@ -46,32 +48,88 @@ int power16(int number) {
     return 16;
 }
 
+double color_to_grey(double values[3]) {
+    return (values[0] + values[1] + values[2]) / 3;
+}
+
+void save_image(char path[], struct image im) {
+    FILE *f;
+    FILE *fgrey;
+    f = fopen(path, "rt");
+    fgrey = fopen("paint2_grey.bmp", "w");
+
+    int c, index, grey_index, i = 0;
+    
+    while (!feof(f) && grey_index * 8 < im.dimensions[0] * im.dimensions[1] * sizeof(double) + 24) {
+        c = fgetc(f);
+        if(index < 54) {
+            fputc(c, fgrey);
+            
+        } else {
+            for (int i = 0; i < 3; i++)
+            {
+                fputc((int)im.values[grey_index], fgrey);
+            }
+            grey_index++;
+        }
+        index++;
+    }
+}
+
 struct image read_image(char path[]){
 
-    struct image image;
-    image.file = fopen(path, "rt");
+    struct image im;
+    im.ndim = 2;
+    FILE *f;
+    f = fopen(path, "rt");
 
-    int c, index = 0;
-    int height, width = 0;
-
-    while (!feof(image.file)) {
-        c = fgetc(image.file);
+    int c, index, grey_index = 0;
+    
+    while (!feof(f)) {
+        c = fgetc(f);
+        // Make sure this is a valid BMP file
         if (index < 2) {
             if(file_header[index] != c) {
                 printf("This is not a correct BMP image");
             }
         }
         index++;
-
+        // Get and Set Dimensions
         if(index > 18 && index <= 22) {
-            width += c * power16(22 - index);
+
+            //im.dimensions[0] += c * power16(22 - index); // not working
+            im.dimensions[0] = 800;
+            // Example : 800 pixels * 800 pixels images will be in hex: 20 03, yet
+            // The int "c" will give 32(10) instead of 20(16).
+            // Basically, 20 * 16 + 3 * 16 * 16 = 800
+            // Or we have, 32 * 16 + 3 * 16 * 16 = 1280
+
         } else if (index > 22 && index <= 26) {
-            height += c * power16(26 - index);
+            
+            //im.dimensions[1] += c * power16(26 - index);
+            im.dimensions[1] = 200;
+        }
+        
+        // Color palette
+
+        // Comp
+
+
+        //values
+        if(index >= 49) { // ???
+            if (index == 49) {
+                im.values = (double *) malloc(im.dimensions[0] * im.dimensions[1] * sizeof(double));
+            }
+            im.values[grey_index] = color_to_grey((double[]){c, fgetc(f), fgetc(f)});
+            grey_index++;
         }
     }
+    return im;
+}
 
-    image.dimensions[0] = width;
-    image.dimensions[1] = height;
-    
-    return image;
+int main() {
+    struct image im = read_image("paint2.bmp");
+    save_image("paint2.bmp", im);
+    free(im.values);
+    return 0;
 }
