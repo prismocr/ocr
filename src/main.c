ff27ed829c27a47bc34d89d4109a7de2d20324db
Aurèle Oulès <aurele@oules.com>
2020-10-03 00:53:33 +0200

    removed leo's garbage

--- src/main.c
+++ src/main.c
@@ -1,9 +1,7 @@
 #include "bitmap.h"
-#include "debug.h"
 #include "error.h"
 #include "image.h"
 #include "matrix.h"
-#include "segmentation.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
