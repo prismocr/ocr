dbf80aee7867b7492c3935498a7a9f9ff3740eb7
Aurèle Oulès <aurele@oules.com>
2020-10-20 01:27:48 +0200

    fixed image_contrast

--- src/image.c
+++ src/image.c
@@ -73,11 +73,10 @@ float clamp(float value, float min, float max) {
 void image_contrast(Matrix *image, float delta) {
     size_t i, j;
     double factor = (259.f * (delta + 255.f)) / (255.f * (259.f - delta));
-
     for (i = 0; i < image->h; i++) {
         for (j = 0; j < image->w; j++) {
             float color = factor * (image->val[i][j] * 255.f - 128) + 128;
-            image->val[i][j] = clamp(color, 0.f, 255.f) / 255.f;
+            image->val[i][j] = clamp(color / 255.f, 0.f, 255.f);
         }
     }
 }
