7dc38ce4c342e08f954edaa235f80aaed51ac65d
TrAyZeN <1810leo@gmail.com>
2020-11-15 21:55:25 +0100

    Improve docs

--- include/imgproc/morphology.h
+++ include/imgproc/morphology.h
@@ -4,13 +4,62 @@
 #include "utils/matrix.h"
 #include <stddef.h>
 
+/**
+ * Creates a new structuring element of size m x n.
+ * It is just a matrix of size m x n filled with 1s.
+ *
+ * @param number of rows of the structuring element.
+ * @param number of columns of the structuring element.
+ * @return structuring element of size m x n.
+ */
 Matrix structuring_element(size_t m, size_t n);
 
+/**
+ * Smooth an image by given structuring element.
+ *
+ * @param image to smooth.
+ * @param structuring element used for smoothing.
+ */
 void smooth(Matrix *image, Matrix kernel);
+
+/**
+ * Dilates an image by a given structuring element.
+ *
+ * param image to dilate.
+ * param structuring element used for dilatation.
+ */
 void dilate(Matrix *image, Matrix kernel);
+
+/**
+ * Erodes an image by a given structuring element.
+ *
+ * param image to erode.
+ * param structuring element used for erosion.
+ */
 void erode(Matrix *image, Matrix kernel);
+
+/**
+ * Opens an image by a given structuring element.
+ *
+ * param image to open.
+ * param structuring element used for opening.
+ */
 void opening(Matrix *image, Matrix kernel);
+
+/**
+ * Closes an image by a given structuring element.
+ *
+ * param image to close.
+ * param structuring element used for closing.
+ */
 void closing(Matrix *image, Matrix kernel);
+
+/**
+ * Computes the difference of two images.
+ *
+ * @param image 1.
+ * @param image 2.
+ */
 void difference(Matrix *image1, Matrix image2);
 
 #endif // MORPHOLOGY_H
