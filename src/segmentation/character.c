5c0d1a70d4fabab8478b0906d1b391830b028c41
Aurèle Oulès <aurele@oules.com>
2020-12-05 12:17:18 +0100

    replaced trim function

--- src/segmentation/character.c
+++ src/segmentation/character.c
@@ -9,10 +9,10 @@
 #include <stdio.h>
 
 int character_segment(Matrix word, MatrixLinkedList *characters) {
-    Matrix trimmed_word;
-    if (image_trim(word, &trimmed_word)) {
-        return 1;
-    }
+    Matrix trimmed_word = trim(&word);
+    // if (image_trim(word, &trimmed_word)) {
+    //     return 1;
+    // }
 
     Matrix trimmed_word_copy;
     matrix_copy(trimmed_word, &trimmed_word_copy);
@@ -28,13 +28,14 @@ int character_segment(Matrix word, MatrixLinkedList *characters) {
     size_t left = 0;
     size_t j;
     for (j = 0; j < word_hist.size; j++) {
-        if (word_hist.val[j] < 1.f || j == word_hist.size-1) {
+        if (word_hist.val[j] < 1.f || j == word_hist.size - 1) {
             if (j >= left + 3) {
-                Matrix character = image_crop(left, 0, j - left, trimmed_word.h,
-                                              trimmed_word_copy);
-                Matrix proc_character = pre_process_char(&character);
+                // Matrix character = image_crop(left, 0, j - left,
+                // trimmed_word.h,
+                //   trimmed_word_copy);
+                Matrix proc_character = pre_process_char(&trimmed_word_copy);
                 mll_insert(characters->length, proc_character, characters);
-                matrix_free(&character);
+                // matrix_free(&character);
             }
             left = j;
         }
@@ -46,4 +47,3 @@ int character_segment(Matrix word, MatrixLinkedList *characters) {
 
     return 0;
 }
-
