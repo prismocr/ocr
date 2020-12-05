e8032016dfd3da800904f68ee8624275d1141332
Obeeron <Obeeron@hotmail.com>
2020-12-05 17:26:40 +0100

    print proc before after

--- src/segmentation/character.c
+++ src/segmentation/character.c
@@ -34,8 +34,27 @@ int character_segment(Matrix word, MatrixLinkedList *characters) {
             if (j >= left + 3) {
                 Matrix character = image_crop(left, 0, j - left, trimmed_word.h,
                                               trimmed_word_copy);
+                /*for(size_t i=0; i<character.h; i++){
+                    for(size_t j=0; j<character.w; j++){
+                        float f = character.val[i][j];
+                        if(f>0)
+                            printf("\033[00m");
+                        printf("<>\033[41m");
+                    }
+                    printf("\033[00m\n");
+                }
+                printf("\n");*/
                 Matrix proc_character = pre_process_char(&character);
-
+                /*for(size_t i=0; i<proc_character.h; i++){
+                    for(size_t j=0; j<proc_character.w; j++){
+                        float f = proc_character.val[i][j];
+                        if(f>0)
+                            printf("\033[41m");
+                        printf("<>\033[00m",f);
+                    }
+                    printf("\n");
+                }
+                printf("\n");*/
                 mll_insert(characters->length, proc_character, characters);
 
                 matrix_free(&proc_character);
