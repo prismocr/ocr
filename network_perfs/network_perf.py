1048737cef9e4209c2f2cf7e165d1ee70b05a7f4
Naexys <naexys.naexys@gmail.com>
2020-12-12 21:46:25 +0100

    Small fix

--- network_perfs/network_perf.py
+++ network_perfs/network_perf.py
@@ -2,36 +2,38 @@ import matplotlib.pyplot as plt
 import sys
 
 def main():
-	path = sys.argv[1]
+    path = sys.argv[1]
 	
-	f = open(path, "r")
-	lines = f.read().splitlines()
-	epochs,batch_size,eta,lmbda = lines[0].split(' ')
-	data = [line.split(' ') for line in lines[1:]]
-	training_accuracy, evaluation_accuracy = [], []
+    f = open(path, "r")
+    lines = f.read().splitlines()
+    epochs,batch_size, eta, lmbda = lines[0].split(' ')
+    data = [line.split(' ') for line in lines[1:]]
+    training_accuracy, evaluation_accuracy = [], []
     
-	for d in data:
-		training_accuracy.append(float(d[0]))
-		evaluation_accuracy.append(float(d[1]))
+    for d in data:
+	    training_accuracy.append(float(d[0]))
+	    evaluation_accuracy.append(float(d[1]))
 
-	plt.title(f"epoch:{epochs} batch_size:{batch_size} eta:{eta} lambda:{lmbda}")
+    plt.title(f"epoch:{epochs} batch_size:{batch_size} eta:{eta} lambda:{lmbda}")
 
-	xPos = range(1,len(data)+1)
-	plt.plot(xPos,training_accuracy, label = 'training_accuracy')
-	plt.plot(xPos,evaluation_accuracy, label = 'evaluation_accuracy')
+    xPos = range(1, len(data)+1)
+    plt.plot(xPos, training_accuracy, label = 'training_accuracy')
+    plt.plot(xPos, evaluation_accuracy, label = 'evaluation_accuracy')
 
-	plt.xlabel('epochs')
-	plt.ylabel('accuracy %')
+    plt.xlabel('epochs')
+    plt.ylabel('accuracy %')
 
-	plt.legend()
+    plt.legend()
 
-	minY = min(min(training_accuracy),min(evaluation_accuracy))
-	plt.axis([1, int(epochs), minY, 100])
+    minY = min(min(training_accuracy), min(evaluation_accuracy))
+    plt.axis([1, int(epochs), minY, 100])
 
-	#plt.show()
-	plt.savefig(path[:-3]+'png')
-	f.close()
+    #plt.show()
+    plt.savefig(path[:-3]+"png")
+    f.close()
 
 if len(sys.argv) < 2:
     raise Exception("Performance file path missing.")
-main()
+
+if __name__ == "__main__":
+    main()
