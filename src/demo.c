ed4411fa29a715bd45e461df9819339b1bc3d20b
Obeeron <Obeeron@hotmail.com>
2020-12-13 16:54:19 +0100

    oui

--- src/demo.c
+++ src/demo.c
@@ -821,11 +821,11 @@ int demo_ocr_char(int argc, char *argv[]) {
 int demo_ocr_train() {
     srand(time(NULL));
 
-    N_cfg cfg = {.epochs = 50,
+    N_cfg cfg = {.epochs = 75,
                  .batch_size = 10,
-                 .eta = 0.3f,
-                 .lambda = 5.0f,
-                 .test_data_ratio = 0.3f,
+                 .eta = 0.2f,
+                 .lambda = 0.0f,
+                 .test_data_ratio = 0.0f,
                  .dataset_path = "dataset/",
                  .nb_layers = 3,
                  .layer_sizes
